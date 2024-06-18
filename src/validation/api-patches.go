a420261c66d24491a22e82a991d207379219b1ff
kk <kk>
2024-06-18 09:55:09 +0100

    fix: patching allowing for any type

--- src/validation/api-patches.go
+++ src/validation/api-patches.go
@@ -4,7 +4,7 @@ import (
 	"github.com/r3labs/diff/v3"
 )
 
-func DisplayPatches[T *interface{}](original T, updated T) (diff.PatchLog, error) {
+func DisplayPatches[T any](original T, updated T) (diff.PatchLog, error) {
 	if changelog, err := diff.Diff(original, updated, diff.DisableStructValues(), diff.AllowTypeMismatch(true)); err != nil {
 		return nil, err
 	} else {
