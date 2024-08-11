c2d48c993748acc960026c0dd013813c05997c3d
kk <kk>
2024-08-11 09:56:14 +0100

    feat: value helpers

--- src/cubelify/score-builder.go
+++ src/cubelify/score-builder.go
@@ -20,6 +20,16 @@ func (b *CubelifyScoreBuilder) SetMode(mode string) *CubelifyScoreBuilder {
 	return b
 }
 
+func (b *CubelifyScoreBuilder) AddValue(value float64) *CubelifyScoreBuilder {
+	b.score.Value = b.score.Value + value
+	return b
+}
+
+func (b *CubelifyScoreBuilder) RemoveValue(value float64) *CubelifyScoreBuilder {
+	b.score.Value = b.score.Value - value
+	return b
+}
+
 func (b *CubelifyScoreBuilder) Build() *CubelifyScore {
 	return b.score
 }
