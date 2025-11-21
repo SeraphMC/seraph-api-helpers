df0c57c86cb23f33a7916a4fcf2359efc15d8d04
Clemintina <git@seraph.si>
2025-11-21 20:57:26 +0000

    feat(utils): add `RemoveStringDuplicates` utility function
    Implemented `RemoveStringDuplicates` to eliminate duplicate strings from an array, with optional sorting. This function enhances string manipulation capabilities by providing streamlined and versatile duplicate removal logic.
    
    Signed-off-by: Clemintina <git@seraph.si>

---
+++ src/utils/string-utils.go
@@ -0,0 +1,34 @@
+package utils
+
+import (
+	"slices"
+	"strings"
+)
+
+// RemoveStringDuplicates removes duplicate strings from the given array and returns the result.
+func RemoveStringDuplicates(strArray []string, options struct {
+	Sort bool
+}) []string {
+	for i, grant := range strArray {
+		strArray[i] = strings.ToLower(strings.TrimSpace(grant))
+	}
+
+	// Sort and compact the array
+	if options.Sort {
+		slices.Sort(strArray)
+		return slices.Compact(strArray)
+	}
+
+	// Compact requires sorting, so i'll need to manually remove duplicates
+	strMap := make(map[string]bool)
+	newStrArray := make([]string, 0, len(strArray))
+
+	// Add strings to a map, if the string has been seen before, consider it a duplicate
+	for _, str := range strArray {
+		if !strMap[str] {
+			strMap[str] = true
+			newStrArray = append(newStrArray, str)
+		}
+	}
+	return newStrArray
+}
